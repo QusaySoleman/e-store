27f0a09e0117e08911406efa6e0da85eece06542
Qusay <qusaymheo1997@gmail.com>
2024-11-06 14:28:23 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -15,6 +15,8 @@ import {
 function App() {
   return (
     <div>
+      {/* Add Repo name --> coffee_store */}
+      {/* basename="/repo name" */}
       <BrowserRouter>
         <Navbar />
         <Routes>
